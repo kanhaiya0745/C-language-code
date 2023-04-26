16048ebcf6565b3930c137e1681fcb437e13bf4d
KRISHNA KANHAIYA <124788574+kanhaiya0745@users.noreply.github.com>
2023-04-26 18:33:36 +0530

    Add files via upload

---
+++ chapter_2 instructions_operators/03practise.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main (){
+    int x=2,y=3;
+    printf("the value of 3*x-8*y is %d \n", 3*x-8*y);
+    printf("the value of 8*y\3*x is %d\n",8*x/3*y);
+    return(0);
+}
---
+++ chapter_2 instructions_operators/basic.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main (){
+    int a, b=a;
+    int v= 3^3;
+    char dt = '2';
+    float d =(3.0/8-2);
+    printf("%d\n",v);
+    printf("%d\n",d);
+
+
+    return(0);
+}
